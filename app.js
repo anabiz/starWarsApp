0755febe4e8f306633d11265e0203f1f773b8ac6
apple <apple@Anabiz.local>
2020-07-23 22:21:08 -0700

    add click event to names

--- app.js
+++ app.js
@@ -21,30 +21,83 @@ let loadHTMLTable = (data)=>{
     }
 
     let tableHtml = "";
-    data1.forEach(function(Name){
+    data1.forEach(function(Name, id){
         tableHtml += "<div class='char'>";
 
         tableHtml += `<div class='imagName'>`;
         tableHtml += `<img class='imag' src="images/dummy.png" >`;
-        tableHtml += `<h3 class='name'>${Name.name}</h3>`;
+        tableHtml += `<h3 class='name' data-id='${id}'>${Name.name}</h3>`;
         tableHtml += `</div>`;
 
         tableHtml += `<div class='genderHeight'>`;
-        tableHtml += `<p>`;
+        tableHtml += `<p id='height'>`;
         tableHtml += `</p>`;
-        tableHtml += `<p>`;
+        tableHtml += `<p id='gender'>`;
         tableHtml += `</p>`;
         tableHtml += `</div>`;
 
         tableHtml +=  "</div>";
 
+        console.log(id);
     });
     characters.innerHTML = tableHtml;
 }
 
+const instance = null;
+class dbService {
+    static getDbServiceInstance(){
+        return instance ? instance : new dbService();
+    }
+    async getAllData(singleUserDetails){
+        try{
+            const response = await new Promise((resolve, reject)=>{
+                
+                if(!singleUserDetails){
+                    reject(new Error(err.message));
+                } else{ 
+                    resolve({Name:singleUserDetails.name, Gender:singleUserDetails.gender, Height:singleUserDetails.height});
+                }
+            });
+           console.log(response.Name);
+            return response;
 
+        }catch(error){
+            console.log(error);
+        }
+    } 
+    
+}
 
-document.querySelector('.name').addEventListener('click', function(event){
+$(document).on('click', '.name', function () {
+    fetch('https://swapi.dev/api/people')
+    .then(response => response.json())
+    .then(data => {
+
+   let $fd=$(this).attr('data-id');
+   console.log($fd);
+   console.log(data.results[$fd]);
+   const db =  dbService.getDbServiceInstance();
+    const result = db.getAllData(data.results[$fd]);
+    console.log(result);
+
+    document.getElementsByClassName('genderHeight')[$(this).attr('data-id')].innerHTML=`<p>${result.promiseValue.Height}</p>${result.Gender}<p></P>`;
+
+});
+   /*
+   localStorage.setItem('mybutt', JSON.stringify($fd));
+   window.location.href="profile.html";
+   */
+   
+
+});
+/*
+ let showdetails = document.querySelectorAll('.name');
+ if(showdetails){
+     showdetails.addEventListener('onclick', ()=>{
+         console.log( "yeessss");
+     });
+ }
+document.querySelector('#list .char .name').addEventListener('click', function(event){
     console.log(event.target);
     if(event.target.className ==="delete-row-btn"){
         deleteRowById(event.target.dataset.id);
@@ -53,3 +106,4 @@ document.querySelector('.name').addEventListener('click', function(event){
         editRowById(event.target.dataset.id)
     }
 });
+*/
